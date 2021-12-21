15e0351ffaf2be89566c01f4ca7da705056b0d09
Rodrigo <rodrigocj>
2021-12-21 16:55:09 -0300

    correcao

--- dist/index.js
+++ dist/index.js
@@ -8444,9 +8444,18 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
+const fetch = __nccwpck_require__(467);
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
+
+    const randomPos = Math.round(Math.random() * 1000);
+    const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+    const response = await fetch(url);
+    const { results } = await response.json();
+    const gifUrl = results[0].media[0].tinygif.url;
+
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
@@ -8454,8 +8463,8 @@ async function run() {
     
     await octokit.rest.issues.createComment({
       ...context.repo,
-  issue_number: pull_request.number,
-      body: "Hello, World!",
+      issue_number: pull_request.number,
+      body: 'Vlw pela pull request, vamos analiza-la o mais rapido possivel\n\n<img src="${gifUrl}" alt="thank you" />',
     });
   }
 
