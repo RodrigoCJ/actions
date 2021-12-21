ad99eb779f01cf3e782f30c780d6bc454b325669
Rodrigo <rodrigocj>
2021-12-21 17:44:37 -0300

    correcao

--- dist/index.js
+++ dist/index.js
@@ -8450,15 +8450,10 @@ async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 
-
-    console.log("token tenor "+TENOR_TOKEN);
     const randomPos = Math.round(Math.random() * 1000);
     const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-    console.log("url "+url);
     const response = await fetch(url);
     const { results } = await response.json();
-    console.log("ee  "+results);
-    console.log("we "+JSON.stringify(object));
     const gifUrl = results[0].media[0].tinygif.url;
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
